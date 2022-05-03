3a0180c4a732e82b13789b771a9a28cac4d2914a
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2022-05-02 21:06:02 -0300

    tentativa da 6

--- q06/q06.c
+++ q06/q06.c
@@ -2,4 +2,19 @@
 
 #include <stdio.h>
 #include <string.h>
+#define TAM 20
 
+int main(){
+    char str[TAM];
+    
+    printf("Digite a string: ");
+    scanf("%s", &str);
+
+    for(int i = 0; i <= TAM; i++){
+        str[i] = str[TAM - i];
+    }
+
+    printf("String inversa: %s", str);
+
+    return 0;
+}
